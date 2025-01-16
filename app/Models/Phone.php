18a15c57ed4b9bd27b84d40f83307ea44f3ba909
nisaja26 <nissaikarachmawati@gmail.com>
2025-01-16 11:31:13 +0700

    membuat relasi one the one

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Phone extends Model
 {
     use HasFactory;
+    
+    /**
+     * user
+     *
+     * @return void
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
+// kita menambahkan user dan di dalamnya melakukan ineverse / belongs to take dalam Model user. 
+// fungsi memberitahu laravel bahwa model phone dimiliki user 
+// relasi one the one sudah berhasil dibuat
+// kita akan mencoba menampilkannya kita menggunakkan controller dan view
