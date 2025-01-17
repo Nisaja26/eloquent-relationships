365a6c867ebe961cf0c84bf73ad8559197b6ce25
nisaja26 <nissaikarachmawati@gmail.com>
2025-01-17 13:42:48 +0700

    Membuat Relasi One to Mony

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    
+    /**
+     * post
+     *
+     * @return void
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
+// menambahkan method baru dengan nama post lalu melakukan relasi belongTo ke dalam odel post
+// memberi tahu laravel bahwa model comment terhubung model post
+// relasi One to Mony
