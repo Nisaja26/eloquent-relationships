bcd0511bbea9395ede852bfb8daef32510738cb5
nisaja26 <nissaikarachmawati@gmail.com>
2025-01-17 13:34:38 +0700

    Membuat relasi One to Many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    /**
+     * comments
+     *
+     * @return void
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    // membuat method baru comments melakukan relasi hasMany ke model comment
 }
